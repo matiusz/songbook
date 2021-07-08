a46d458d1e346e5c34b7eb5847491c1d46464721
matiusz <matszwecja@gmail.com>
2021-07-08 10:44:41 +0200

    vert spacing, code cleanup, category image preview

--- songJSONmaker.py
+++ songJSONmaker.py
@@ -2,6 +2,8 @@ from PySide6.QtWidgets import (QApplication, QWidget, QLineEdit,
                                 QHBoxLayout, QVBoxLayout, QPlainTextEdit, 
                                 QPushButton,QScrollArea, QLayout,  
                                 QComboBox, QFileDialog, QLabel)
+from PySide6.QtGui import QPixmap
+from PySide6.QtCore import QSize, Qt
 
 import json
 
@@ -9,16 +11,49 @@ import os
 
 from functools import partial
 
-def ensure_file_dir(file_path):
+def ensureFileDir(file_path):
     directory = os.path.dirname(file_path)
     if not os.path.exists(directory):
         os.makedirs(directory)
 
-def create_dir(directory):
+def ensureDir(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
 
+def getCategoriesFromDirs():
+    categories = []
+    for dirname in os.listdir():
+        if os.path.isdir(dirname) and not dirname.startswith("."):
+            categories.append(dirname)
+    return categories
 
+
+class MainMenu(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Songbook Maker')
+
+        image = QPixmap("logo")
+        label = QLabel()
+        label.setPixmap(image)
+
+        categoryButton = QPushButton('New Category', self)
+        categoryButton.clicked.connect(self.addCategoryField)
+        
+        songButton = QPushButton('New Song', self)
+        songButton.clicked.connect(self.addSongField)
+
+        layout = QVBoxLayout()
+        layout.addWidget(label)
+        layout.addWidget(categoryButton)
+        layout.addWidget(songButton)
+        self.setLayout(layout)
+        self.show()
+
+    def addSongField(self):
+        self.currentSong = ScrollableSong()
+    def addCategoryField(self):
+        self.currentCat = NewCategory()
 
 
 class NewCategory(QWidget):
@@ -26,30 +61,39 @@ class NewCategory(QWidget):
         super().__init__()
         self.setWindowTitle('New Category')
         self.setGeometry(200, 100, 500, 500)
+
+        layout = QVBoxLayout()
+        
         self.name = QLineEdit()
         self.name.setPlaceholderText("Category Name")
+        layout.addWidget(self.name)
+
         self.label = QLabel()
         self.label.setText("Choose image for the category")
+        layout.addWidget(self.label)
+
         self.image = QFileDialog()
-        self.image.setNameFilter("*.py *.jpg *.png *.bmp")
+        self.image.setNameFilter("*.jpg *.png *.bmp")
         self.image.setFileMode(QFileDialog.ExistingFile)
-        self.image.fileSelected.connect(lambda: self.update_label())
+        self.image.fileSelected.connect(lambda: self.updateLabel())
+        layout.addWidget(self.image)
+
         closeButton = QPushButton('Save&Quit', self)
         closeButton.clicked.connect(lambda: self.close())
-        layout = QVBoxLayout()
-        layout.addWidget(self.name)
-        layout.addWidget(self.label)
-        layout.addWidget(self.image)
         layout.addWidget(closeButton)
+
         self.setLayout(layout)
         self.show()
-    def update_label(self):
-        self.label.setText(self.image.selectedFiles()[0])
-        self.setGeometry(200, 100, 100, 100)
+
+    def updateLabel(self):
+        image = QPixmap(self.image.selectedFiles()[0])
+        size = QSize(500, 500)
+        image = image.scaled(size, Qt.KeepAspectRatio)
+        self.label.setPixmap(image)
     def closeEvent(self, event):
         category = self.name.text()
         if category:
-            create_dir(category)
+            ensureDir(category)
             image = self.image.selectedFiles()[0]
             if image:
                 _, file_extension = os.path.splitext(image)
@@ -59,26 +103,6 @@ class NewCategory(QWidget):
                 image_from.close()
                 image_to.close()
 
-class MainMenu(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.songs = []
-        self.categories = []
-        self.setGeometry(100, 100, 200, 150)
-        self.setWindowTitle('Songbook Maker')
-        songButton = QPushButton('New Song', self)
-        songButton.clicked.connect(self.add_song_field)
-        songButton.move(50, 50)
-        categoryButton = QPushButton('New Category', self)
-        categoryButton.clicked.connect(self.add_category_field)
-        categoryButton.move(50, 75)
-        self.show()
-    def add_song_field(self):
-        self.songs.append(ScrollableSong())
-    def add_category_field(self):
-        self.categories.append(NewCategory())
-        
-        
 
 class ScrollableSong(QScrollArea):
     def __init__(self):
@@ -89,51 +113,54 @@ class ScrollableSong(QScrollArea):
         self.setWidget(self.song)
         self.setWidgetResizable(True)
         self.show()
+
     def closeEvent(self, event):
         preJSON = self.widget().toJSON()
         path = os.path.join(preJSON['category'], preJSON['title'] + ".sng")
-        ensure_file_dir(path)
+        ensureFileDir(path)
         f = open(path, "w")
         f.write(json.dumps(preJSON))
         event.accept()
-
-def getCategoriesFromDirs():
-    categories = []
-    for dirname in os.listdir():
-        if os.path.isdir(dirname) and not dirname.startswith("."):
-            categories.append(dirname)
-    return categories
 
 
 class Song(QWidget):
     def __init__(self, parent):
         super().__init__()
         self.parent = parent
-        layout = QVBoxLayout()
-        layout.setSizeConstraint(QLayout.SetMinimumSize)
-        self.catBar = QComboBox()
-        self.catBar.addItems(getCategoriesFromDirs())
-        layout.addWidget(self.catBar)
-        self.titleBar = QLineEdit()
-        self.titleBar.setPlaceholderText("Song Title")
-        layout.addWidget(self.titleBar)
-        btnVerse = QPushButton('New Verse', self)
-        btnVerse.clicked.connect(self.newSection)
-        btnChorus = QPushButton('New Chorus', self)
-        btnChorus.clicked.connect(partial(self.newSection, chorus = True))
-        btnClose = QPushButton('Save&Quit', self)
-        btnClose.clicked.connect(lambda: self.parent.close())
-        #btn_quit.resize(btn_quit.sizeHint())
-        buttonBox = QHBoxLayout()
-        buttonBox.addWidget(btnVerse)
-        buttonBox.addWidget(btnChorus)
-        layout.addLayout(buttonBox)
-        layout.addWidget(btnClose)
         self.setGeometry(300, 100, 500, 100)
         self.setWindowTitle('Song Field')
         self.sections = []
+
+        layout = QVBoxLayout()
+        layout.setSizeConstraint(QLayout.SetMinimumSize)
+        
+        self.catBar = QComboBox()
+        self.catBar.addItems(getCategoriesFromDirs())
+        layout.addWidget(self.catBar)
+        
+        self.titleBar = QLineEdit()
+        self.titleBar.setPlaceholderText("Song Title")
+        layout.addWidget(self.titleBar)
+        
+        buttonBox = QHBoxLayout()
+        
+        verseButton = QPushButton('New Verse', self)
+        verseButton.clicked.connect(self.newSection)
+        buttonBox.addWidget(verseButton)
+        
+        chorusButton = QPushButton('New Chorus', self)
+        chorusButton.clicked.connect(partial(self.newSection, chorus = True))
+        buttonBox.addWidget(chorusButton)
+        
+        layout.addLayout(buttonBox)
+        
+        closeButton = QPushButton('Save&Quit', self)
+        closeButton.clicked.connect(lambda: self.parent.close())
+        layout.addWidget(closeButton)
+        
         self.setLayout(layout)
         self.show()
+
     def newSection(self, chorus = False):
         newSection = SongSection(chorus = chorus)
         self.sections.append(newSection)
@@ -165,6 +192,7 @@ class SongSection(QHBoxLayout):
             stretches = (75, 25)
         self.addWidget(self.lyrics, stretches[0])
         self.addWidget(self.chords, stretches[1])
+
     def toJSON(self):
         jsonSection = {}
         jsonSection['chords'] = self.chords.toPlainText()
@@ -176,7 +204,7 @@ if __name__=="__main__":
     import sys
     app = QApplication(sys.argv)
     window = MainMenu()
-    create_dir(".images")
+    ensureDir(".images")
     app.exec()
     f = open("categories.cfg", "wb")
     for dirname in os.listdir():
--- songbook.py
+++ songbook.py
@@ -2,6 +2,11 @@ import os
 import json
 from collections import defaultdict
 
+def en_utf8(st):
+    return st.encode('utf-8')
+def de_utf8(st):
+    return st.decode('utf-8')
+
 def songToTex(songJSON):
     songStr = "\\section*{{{title}}}\n\\addcontentsline{{toc}}{{section}}{{{title}}}\n\\columnratio{{0.7,0.3}}\n".format(title=songJSON['title'])
     for section in songJSON['sections']:
@@ -42,37 +47,45 @@ def categoryToTex(category):
         "\\newpage\n"
     return catStr
 
-if __name__=="__main__":
-    headfileName = "latexheader.txt"
-    headfile = open(headfileName, "rb")
-    songbookFileName = "songbook.tex"
-    if os.path.exists(songbookFileName):
-        os.remove(songbookFileName)
-    songbookFile = open(songbookFileName, "ab")
-    songbookFile.write(headfile.read())
+
+def main():
+    configFilename = "categories.cfg"
+    headerFilename = "latexheader.txt"
+    headerFile = open(headerFilename, "rb")
+    songbookFilename = "songbook.tex"
+    if os.path.exists(songbookFilename):
+        os.remove(songbookFilename)
+    songbookFile = open(songbookFilename, "ab")
+    songbookFile.write(headerFile.read())
+    headerFile.close()
+    
     categories = defaultdict(lambda: {})
-    headfile.close()
 
     for dirname in os.listdir():
         if os.path.isdir(dirname) and not dirname.startswith("."):
             for filename in os.listdir(dirname):
                 if filename.endswith(".sng"):
-                    songfile = open(os.path.join(dirname, filename))
-                    song = json.loads(songfile.read())
+                    songFile = open(os.path.join(dirname, filename))
+                    song = json.loads(songFile.read())
                     categories[song['category']][song['title']] = song
 
     if os.path.exists("categories.cfg"):
-        f = open("categories.cfg", "rb")
-        cats_text = f.read().decode("utf-8")
+        configFile = open("categories.cfg", "rb")
+        cats_text = de_utf8(configFile.read())
         cats = cats_text.splitlines()
+        
+    cats = [cat for cat in cats if not cat.startswith("#")]
 
-    cats = cats or categories.keys()
+    cats = cats or sorted(categories.keys())
     
     for cat in cats:
         print(cat)
-        songbookFile.write(categoryToTex(cat).encode('utf-8'))
+        songbookFile.write(en_utf8(categoryToTex(cat)))
         for song in sorted(categories[cat].keys()):
             print("\t" + song)
-            songbookFile.write(songToTex(categories[cat][song]).encode('utf-8'))
-    songbookFile.write("\end{document}".encode('utf-8'))
+            songbookFile.write(en_utf8(songToTex(categories[cat][song])))
+    songbookFile.write(en_utf8("\end{document}"))
     songbookFile.close()
+
+if __name__=="__main__":
+    main()
