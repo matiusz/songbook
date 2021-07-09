396c30cbb1fdf1921395e016228a1f3b9cf08daf
matiusz <matszwecja@gmail.com>
2021-07-09 14:14:10 +0200

    Aux file cleanup in automated script

--- songbook.py
+++ songbook.py
@@ -11,3 +11,13 @@ import os
 texcompile = "pdflatex songbook.tex"
 os.system(texcompile)
 os.system(texcompile)
+
+auxFiles = ["songbook.tex", "songbook.toc", "songbook.aux", "songbook.fdb_latexmk", "songbook.ffs", "songbook.log", "songbook.synctex.gz", "songbook.fls"]
+
+for auxFil in auxFiles:
+    try:
+        os.remove(auxFil)
+    except FileNotFoundError:
+        pass
+    except Exception as ex:
+        raise ex
