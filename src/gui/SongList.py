a6ad34a010340f09b95d626bb584aaa8a34016c7
matiusz <matszwecja@gmail.com>
2021-09-28 17:19:57 +0200

    Added checkbox to search in contents for display

--- src/gui/SongList.py
+++ src/gui/SongList.py
@@ -1,4 +1,5 @@
-from PySide6.QtWidgets import (QWidget, QLineEdit, QVBoxLayout, 
+import os
+from PySide6.QtWidgets import (QCheckBox, QHBoxLayout, QWidget, QLineEdit, QVBoxLayout, 
                                 QScrollArea, QLabel)
 from PySide6.QtCore import Qt
 
@@ -17,17 +18,44 @@ class ScrollAndSearchSongList(QWidget):
 
         self.scrollableSongList = ScrollableSongList()
 
-        self.filter = QLineEdit()
-        self.filter.setPlaceholderText("Search...")
-        self.filter.textChanged.connect(self.scrollableSongList.songList.reloadText)
+
+        self.filterBar = SearchBar(self)
 
 
-        layout.addWidget(self.filter)
+        layout.addWidget(self.filterBar)
         layout.addWidget(self.scrollableSongList)
 
         self.setLayout(layout)
 
         self.show()
+
+class SearchBar(QWidget):
+    def __init__(self, parent):
+        super().__init__()
+        self.parent = parent
+        layout = QHBoxLayout()
+
+        self.filter = QLineEdit()
+        self.filter.setPlaceholderText("Search...")
+        self.filter.textChanged.connect(self.filt)
+
+        self.desc = QLabel()
+        self.desc.setText("Search in contents: ")
+
+        self.checkBox = QCheckBox()
+        self.checkBox.stateChanged.connect(self.searchInContent)
+
+        layout.addWidget(self.filter)
+        layout.addWidget(self.desc)
+        layout.addWidget(self.checkBox)
+
+        self.setLayout(layout)
+
+    def filt(self, filterText):
+        self.parent.scrollableSongList.songList.reloadText(filterText, self.checkBox.isChecked())
+    def searchInContent(self, detailed = False):
+        self.parent.scrollableSongList.songList.reloadText(self.filter.displayText(), detailed)
+
 
 class ScrollableSongList(QScrollArea):
     def __init__(self):
@@ -51,8 +79,8 @@ class SongList(QLabel):
         cat, _, song = link.partition('#')
         ScrollableSongDisplay(cat, song)
 
-    def reloadText(self, filt):
-        self.setText(self.getText(filt))
+    def reloadText(self, filt, detailed = False):
+        self.setText(self.getText(filt, detailed))
         pass
     
     def loadSongs(self):
@@ -61,18 +89,27 @@ class SongList(QLabel):
         for cat in cats:
             self.catSongs[cat] = [song[:-4] for song in dirTools.getSongsFromCatDir(cat)]
 
-    def getText(self, filt = None):
+    def getText(self, filt = None, detailed = False):
         textLines = []
         for cat in self.catSongs.keys():
             catEmpty = True
             textLines.append(f"{cat}")
             for song in self.catSongs[cat]:
+                
+                linkedTitle = f"{'&nbsp;'*8}<a href=\"{cat}#{song}\">{song}</a>"
                 if filt:
                     if filt.lower() in song.lower():
-                        textLines.append(f"{'&nbsp;'*8}<a href=\"{cat}#{song}\">{song}</a>")
+                        textLines.append(linkedTitle)
                         catEmpty = False
+                    else:
+                        if detailed:
+                            with open(os.path.join(os.getcwd(), "data", cat, song + ".sng"), "r") as f:
+                                text = f.read()
+                                if filt.lower() in text.lower():
+                                    textLines.append(linkedTitle)
+                                    catEmpty = False
                 else:
-                    textLines.append(f"{'&nbsp;'*8}<a href=\"{cat}#{song}\">{song}</a>")
+                    textLines.append(linkedTitle)
                     catEmpty = False
             if catEmpty:
                 textLines.pop()
