bb904c8ad44768fa364d2aa2f522ab24d84548ab
matiusz <matszwecja@gmail.com>
2022-01-03 14:41:16 +0100

    maximized window, option to open new tab without switching

--- src/gui/SongTabs.py
+++ src/gui/SongTabs.py
@@ -1,45 +1,61 @@
-from PySide6.QtWidgets import (QSizePolicy, QTabWidget, QWidget, 
-                                QHBoxLayout)
+from PySide6.QtWidgets import (QSizePolicy, QTabWidget, QWidget,
+                               QHBoxLayout)
 
 from src.gui.SongDisplay import QScrollableSongDisplay
 from src.gui.SongList import ScrollAndSearchSongList
 
-
+from pynput import keyboard
 
 class QSongTabs(QWidget):
     def __init__(self):
         super().__init__()
-        
-        self.setGeometry(0, 0, 400, 1000)
-        
+
         layout = QHBoxLayout()
+
         songList = ScrollAndSearchSongList()
         songList.setSizePolicy(QSizePolicy.Maximum, QSizePolicy.Expanding)
-        songList.setMinimumWidth(300)
-        
+        songList.setMinimumWidth(350)
         songList.scrollableSongList.songList.openSongDisplay = self.openSongDisplay
         layout.addWidget(songList)
-        
+
         self.tabWidget = QTabWidget()
         self.tabWidget.setTabsClosable(True)
         self.tabWidget.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        
         layout.addWidget(self.tabWidget)
 
         self.setLayout(layout)
-        
-        self.tabsLoaded = False
 
-        self.tabWidget.tabInserted = self.tabWidget.setCurrentIndex
+        self.tabsLoaded = False
+        self.altPressed = False
+
+        self.tabWidget.tabInserted = self.openSong
         self.tabWidget.tabCloseRequested.connect(self.tabWidget.removeTab)
 
+        self.listener = keyboard.Listener(
+            on_press=self.onPress,
+            on_release=self.onRelease)
+        self.listener.start()
 
         self.repaint()
-        
-        self.show()
-        
+
+        self.showMaximized()
+
+    def closeEvent(self, event):
+        self.listener.stop()
+        event.accept()
+
+    def openSong(self, index):
+        if not self.altPressed:
+            self.tabWidget.setCurrentIndex(index)
+
     def openSongDisplay(self, link):
         cat, _, song = link.partition('#')
         self.tabWidget.addTab(QScrollableSongDisplay(cat, song), song)
-        if not self.tabsLoaded:
-            self.setGeometry(0, 30, 1500, 1000)
+
+    def onPress(self, key):
+        if key in [keyboard.Key.alt, keyboard.Key.alt_l]:
+            self.altPressed = True
+
+    def onRelease(self, key):
+        if key in [keyboard.Key.alt, keyboard.Key.alt_l]:
+            self.altPressed = False
