1c83c5d9ee464d015299d0f36e250b4b6dccec5a
matiusz <matszwecja@gmail.com>
2024-02-01 16:14:09 +0100

    code: Remove unnecessary files

--- src/obj/Songbook.py
+++ src/obj/Songbook.py
@@ -1,9 +1,5 @@
 from .Song import Song
 from src.tools.loggerSetup import logging
-from collections import defaultdict
-from unidecode import unidecode
-from src.tools import fuzz
-from string import punctuation
 from src.tools.dirTools import getCategoriesFromDirs, getSongFilenamesFromCatDir
 
 logger = logging.getLogger(__name__)
@@ -16,19 +12,3 @@ class Songbook:
             for song in self.sb[cat]:
                 if song.category != cat:
                     logger.warning(f"Category mismatch for song {song.title} - category: {song.category}, folder: {cat}")
-
-    def filteredSongs(self, filter):
-        filtered = defaultdict(list)
-        for cat, songs in self.sb.items():
-            for song in songs:
-                if not filter or \
-                    prepareSearchString(filter) in prepareSearchString(song.title) or \
-                    fuzz.partial_ratio(prepareSearchString(filter), prepareSearchString(song.filterString)) > 95:
-                    filtered[cat].append(song)
-        return filtered
-    
-def prepareSearchString(string):
-    string = string.lower()
-    string = unidecode(string)
-    string = string.translate(str.maketrans('', '', punctuation))
-    return string
