aa2f567846874e156755586e1b496f83a234d8aa
mwmajewsk <5279578+mmajewsk@users.noreply.github.com>
2024-06-18 23:37:48 +0200

    Hardcore changes

--- songbook/src/obj/Songbook.py
+++ songbook/src/obj/Songbook.py
@@ -1,19 +1,20 @@
 from .Song import Song
 from ..tools.loggerSetup import logging
 from ..tools.dirTools import getCategoriesFromDirs, getSongFilenamesFromCatDir
+import os
 
 logger = logging.getLogger(__name__)
 
 class Songbook:
     def __init__(self):
         self.sb: dict[str, list[Song]] = {}
+        # hack this shit
         with open(os.path.join(config.dataFolder, config.categoriesFile), "rb") as catConfig:
             changes = json.loads(catConfig.read())
         self.changed_name = changes['names']
         self.changed_id = changes['place']
         cats = getCategoriesFromDirs()
         for cat in cats:
-            # hack this shit
             self.sb[cat] = [Song.loadFromCatAndTitle(cat, songFilename[:-4]) for songFilename in getSongFilenamesFromCatDir(cat)]
             for song in self.sb[cat]:
                 if song.category != cat:
